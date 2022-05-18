bbd4bb0b09faf684d0bb26197b782ee6cfc188c5
Dorian Dubreus <doriandubreus@gmail.com>
2022-05-18 12:04:50 -0400

    game works

--- js/example.js
+++ js/example.js
@@ -25,3 +25,30 @@ function lastLetter(word) {
     return wordArr[word.length - 1]
 
 }
+
+const results = document.querySelector('#results');
+
+function calculate() {
+    for (let i = 1; i < 10; i++) {
+        const newResult = `${i} x ${i} = ${i * i} | `;
+        results.textContent += `${newResult} \n`;
+    }
+    results.textContent += '\n...finished!';
+}
+
+const calculateBtn = document.querySelector('#calculate');
+const clearBtn = document.querySelector('#clear');
+
+calculateBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', () => results.textContent = '');
+
+let cats = ['ancine', 'sphyx', 'stick']
+
+let myFavorite = 'my favorite cats are'
+
+for (const cat in cats) {
+    myFavorite = `${myFavorite}${cat}`
+
+}
+
+console.log(myFavorite)
